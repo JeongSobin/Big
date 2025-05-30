f30a5848af2d4c17c48c4c401ba4048418473d45
JeongSobin <sssin10@naver.com>
2025-05-30 17:43:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = 'todo-pwa-v1';
+const CACHE = 'pwa-cache-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
+  '/', '/index.html',
+  '/style.css', '/app.js',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icons/icon-192.png','/icons/icon-512.png'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME)
+self.addEventListener('install', e =>
+  e.waitUntil(
+    caches.open(CACHE)
       .then(cache => cache.addAll(ASSETS))
       .then(() => self.skipWaiting())
-  );
-});
+  )
+);
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request)
-      .then(res => res || fetch(evt.request))
-  );
-});
+self.addEventListener('fetch', e =>
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  )
+);
