665db31835b21d5c151439d5caf2d526d2501224
JeongSobin <sssin10@naver.com>
2025-05-30 18:35:08 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,41 @@
-const CACHE = 'pwa-cache-v1';
+const CACHE_NAME = 'pwa-cache-v1';
 const ASSETS = [
-  './',             // the root of this site (same as index.html)
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
 ];
 
-self.addEventListener('install', e =>
-  e.waitUntil(
-    caches.open(CACHE)
+// 설치 이벤트: 필수 리소스 캐싱
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
       .then(() => self.skipWaiting())
-  )
-);
+  );
+});
 
-self.addEventListener('fetch', e =>
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => res || fetch(e.request))
-  )
-);
+// 활성화 이벤트: 이전 캐시 정리
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 요청 가로채기: 캐시 우선
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(cachedRes => cachedRes || fetch(evt.request))
+  );
+});
