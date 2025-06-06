ae7207be52298e0a3bdba1d619d8f425f4e99679
sieun-oh <osieun80@naver.com>
2025-06-06 14:33:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 function showScreen(id) {
   document.querySelectorAll('.screen')
     .forEach(el => el.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
+  document.getElementById(id)?.classList.add('active');
 }
 
 window.addEventListener('DOMContentLoaded', () => {
